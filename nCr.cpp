87d1157e66cedfd2fba63b3599f2f77fb34c564d
Mr-Gopal <mr.gopal.programmer@gmail.com>
2025-12-25 22:19:39 +0530

    program to calculate combinations

---
+++ nCr.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+long factorial(int num) {
+    long fact = 1;
+    if (num == 0 || num == 1) return 1;
+
+    while (num > 0) {
+        fact *= num;
+        num--;
+    }
+    return fact;
+}
+
+int main(){
+    int n,r;
+    cout << "You got nCr calculator." << endl;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    cout << "Enter the value of r: ";
+    cin >> r;
+
+    if (r > n) {
+        cout << "nCr is not executable!";
+    } else {
+        int nCr = factorial(n)/(factorial(r)*factorial(n-r));
+        cout << "nCr = " << nCr;
+    }
+}
